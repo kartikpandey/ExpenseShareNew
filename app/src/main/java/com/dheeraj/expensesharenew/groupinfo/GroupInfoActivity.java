287bdd11e91b44ff9709be40a53d98c6d51dfd0d
kartikpandey <dheeraj.pandey23995@gmail.com>
2020-01-04 13:55:57 +0530

    created groupInfo adapter

--- app/src/main/java/com/dheeraj/expensesharenew/groupinfo/GroupInfoActivity.java
+++ app/src/main/java/com/dheeraj/expensesharenew/groupinfo/GroupInfoActivity.java
@@ -9,8 +9,10 @@ import android.os.Bundle;
 import com.dheeraj.expensesharenew.BaseActivity;
 import com.dheeraj.expensesharenew.R;
 import com.dheeraj.expensesharenew.groupDetail.GroupDetailActivity;
+import com.dheeraj.expensesharenew.groupdashboard.GroupMember;
 import com.dheeraj.expensesharenew.groupdashboard.GroupModel;
 import com.dheeraj.expensesharenew.groupdashboard.adapter.GroupDashboardAdapter;
+import com.dheeraj.expensesharenew.groupinfo.adapter.GroupInfoAdapter;
 
 import java.util.ArrayList;
 
@@ -22,18 +24,28 @@ public class GroupInfoActivity extends BaseActivity {
     @BindView(R.id.recyclerViewMembers)
     RecyclerView recyclerViewMembers;
 
+    GroupInfoAdapter groupInfoAdapter;
+
+    static GroupInfoActivity groupInfoActivity;
+
+    public static GroupInfoActivity getInstance(){
+        return groupInfoActivity;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_info);
 
         ButterKnife.bind(this);
+        groupInfoActivity = this;
+        setGroupListData(GroupDetailActivity.groupDetail.getGroupMembersList());
     }
 
-    void setGroupListData(ArrayList<GroupModel> groupList) {
-//        groupDashboardAdapter = new GroupDashboardAdapter(GroupDetailActivity.groupDetail.getGroupMembersList());
-//        recyclerViewMembers.setHasFixedSize(true);
-//        recyclerViewMembers.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerViewMembers.setAdapter(groupDashboardAdapter);
+    void setGroupListData(ArrayList<GroupMember> groupMembersList) {
+        groupInfoAdapter = new GroupInfoAdapter(groupMembersList);
+        recyclerViewMembers.setHasFixedSize(true);
+        recyclerViewMembers.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewMembers.setAdapter(groupInfoAdapter);
     }
 }
--- app/src/main/java/com/dheeraj/expensesharenew/groupinfo/adapter/GroupInfoAdapter.java
+++ app/src/main/java/com/dheeraj/expensesharenew/groupinfo/adapter/GroupInfoAdapter.java
@@ -1,6 +1,93 @@
 package com.dheeraj.expensesharenew.groupinfo.adapter;
 
-public class GroupInfoAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dheeraj.expensesharenew.R;
+import com.dheeraj.expensesharenew.groupdashboard.GroupMember;
+import com.dheeraj.expensesharenew.groupdashboard.GroupModel;
+import com.dheeraj.expensesharenew.groupinfo.GroupInfoActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class GroupInfoAdapter extends RecyclerView.Adapter<GroupInfoAdapter.ViewHolder> {
+
+    private final List<GroupMember> groupMembersList;
+    private List<GroupMember> groupMembersListFiltered;
+    private Context context;
+    private View view;
+    String customerName;
+
+    public GroupInfoAdapter(ArrayList<GroupMember> groupMembersList) {
+        this.groupMembersList = groupMembersList;
+        this.groupMembersListFiltered = groupMembersList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.group_info_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.textViewMemberName.setText(groupMembersListFiltered.get(position).getMemberName());
+        if(groupMembersListFiltered.get(position).getMemberType().equals(GroupInfoActivity.getInstance().getResources().getString(R.string.admin))){
+            holder.textViewMemberType.setVisibility(View.VISIBLE);
+            holder.textViewMemberType.setText(groupMembersListFiltered.get(position).getMemberType());
+        }else {
+            holder.textViewMemberType.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return groupMembersListFiltered.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final View mView;
+
+        @BindView(R.id.textViewMemberName)
+        TextView textViewMemberName;
+
+        @BindView(R.id.textViewMemberType)
+        TextView textViewMemberType;
+
+        @BindView(R.id.relativeLayoutMember)
+        RelativeLayout relativeLayoutMember;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            ButterKnife.bind(this, view);
+            relativeLayoutMember.setOnClickListener(this);
+        }
+
+        @Override
+        public String toString() {
+            return "";
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v == relativeLayoutMember) {
+            }
+        }
+    }
 
 }
+
+
