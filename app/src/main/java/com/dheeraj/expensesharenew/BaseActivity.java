70d2a30872e5bfa0a4d4e9c3f547e939b5215f6a
kartikpandey <dheeraj.pandey23995@gmail.com>
2020-01-14 23:17:39 +0530

    Worked on Notifications

--- app/src/main/java/com/dheeraj/expensesharenew/BaseActivity.java
+++ app/src/main/java/com/dheeraj/expensesharenew/BaseActivity.java
@@ -1,14 +1,26 @@
 package com.dheeraj.expensesharenew;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dheeraj.expensesharenew.groupdashboard.GroupDashboardActivity;
+import com.dheeraj.expensesharenew.groupdashboard.GroupModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -22,13 +34,16 @@ public class BaseActivity extends AppCompatActivity {
     public static String KeyMobNo = "mobNo";
     public static String KeyGender = "gender";
     public static String KeyUID = "uID";
-    public static String KeyGroupId= "groupId";
-    public static String KeyGroupName= "groupName";
-    public static String KeyMemberName= "memberName";
-    public static String KeyMemberUid= "memberUid";
-    public static String KeyMemberType= "memberType";
-    public static String KeyNotifications= "notifications";
+    public static String KeyGroupId = "groupId";
+    public static String KeyGroupName = "groupName";
+    public static String KeyMemberName = "memberName";
+    public static String KeyMemberUid = "memberUid";
+    public static String KeyMemberType = "memberType";
+    public static String KeyNotifications = "notifications";
     public static DatabaseReference mdDatabaseReference;
+
+    @BindView(R.id.textViewNotifyCount)
+    TextView textViewNotifyCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +51,7 @@ public class BaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_base);
         setActionBarData();
         mdDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        getNotifications();
     }
 
     public void setActionBarData() {
@@ -59,5 +75,27 @@ public class BaseActivity extends AppCompatActivity {
 
     public void imageButtonNotificationClick(View view) {
 
+    }
+
+    void getNotifications() {
+        mdDatabaseReference.child(KeyNotifications)
+                .child(GroupDashboardActivity.userInfoModel.getuID())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        int notificationCount = 0;
+                        if (dataSnapshot.getValue() != null) {
+                            for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                                notificationCount++;
+                            }
+                        }
+                        textViewNotifyCount.setText(notificationCount + "");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        databaseError.getMessage();
+                    }
+                });
     }
 }
